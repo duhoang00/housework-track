724b38054e62e5964e36afbb4c826db5a9cd925d
Du Hoang <hoangminhdu2000@gmail.com>
2022-07-20 00:11:53 +0700

    update packages & add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <v-app><v-app-bar></v-app-bar></v-app>
+
+    <slot />
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
+  css: [
+    "vuetify/lib/styles/main.sass",
+    // "@mdi/font/css/materialdesignicons.min.css", // should use this instead of below scss line
+    "@mdi/font/scss/materialdesignicons.scss",
+  ],
   build: {
     transpile: ["vuetify"],
   },
